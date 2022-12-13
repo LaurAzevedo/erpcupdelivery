6c06dad3d78240e0841867f7e0eb356e4e7964ee
Laura Azevedo <85031729+LaurAzevedo@users.noreply.github.com>
2022-12-13 15:10:17 -0300

    Implementação da função de validação de CPF

--- Components/controller/GeneralController.vb
+++ Components/controller/GeneralController.vb
@@ -1,4 +1,6 @@
-﻿Module GeneralController
+﻿Imports System.Text.RegularExpressions
+
+Module GeneralController
 
     Public CloseSplashScreen As Boolean = False
     Public AppDataFolder As String = "C:\ProgramData\"
@@ -30,5 +32,54 @@
         System.Threading.Thread.Sleep(0.001)
         Application.DoEvents()
     End Sub
+
+    Public Function ValidateCpf(ByVal CPF As String) As Boolean
+
+        'Remove caracteres inválidos do CPF
+        CPF = Regex.Replace(CPF, "[^0-9]", "")
+
+        'Verifica se o CPF tem 11 dígitos
+        If CPF.Length <> 11 Then
+            Return False
+        End If
+
+        'Calcula o primeiro dígito verificador
+
+        Dim sum As Integer = 0
+
+        For i As Integer = 0 To 8
+            sum += Integer.Parse(CPF.Substring(i, 1)) * (10 - i)
+        Next
+
+        Dim firstDigit As Integer = 11 - (sum Mod 11)
+
+        If firstDigit >= 10 Then
+            firstDigit = 0
+        End If
+
+        'Calcula o segundo dígito verificador
+
+        sum = 0
+
+        For i As Integer = 0 To 8
+            sum += Integer.Parse(CPF.Substring(i, 1)) * (11 - i)
+        Next
+
+        sum += firstDigit * 2
+
+        Dim secondDigit As Integer = 11 - (sum Mod 11)
+
+        If secondDigit >= 10 Then
+            secondDigit = 0
+        End If
+
+        'Verifica se os dígitos verificadores calculados são iguais aos dígitos verificadores do CPF informado
+        If firstDigit = Integer.Parse(CPF.Substring(9, 1)) AndAlso secondDigit = Integer.Parse(CPF.Substring(10, 1)) Then
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
 
 End Module
